91ff4f1352212573f48fba47cb9072c965816114
MuYvs <MuhammadYvsvf@gmail.com>
2025-04-11 16:17:02 +0200

    Added a choice comparison function, global scores.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
 
+let player_score = 0;
+let computer_score = 0;
 function CompChoice(){
     let choice_num = random(3);
     let choice;
@@ -27,4 +29,42 @@ function PlayerChoice() {
     }
 }
 
-console.log(PlayerChoice())
+function CompareChoice (p_choice, c_choice) {
+    if (p_choice === 'rock' && c_choice === 'rock') {
+        alert(`Tie! Both You and the Computer Chose 'rock'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+    else if (p_choice === 'rock' && c_choice === 'paper') {
+        computer_score =+ 1;
+        alert(`You Lose! You Chose 'rock' and the Computer Chose 'paper'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+    else if (p_choice === 'rock' && c_choice === 'scissor') {
+        player_score =+ 1;
+        alert(`You Win! You Chose 'rock' and the Computer Chose 'scissor'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+
+    else if (p_choice === 'paper' && c_choice === 'rock') {
+        player_score += 1;
+        alert(`You Win! You Chose 'paper' and the Computer Chose 'rock'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+    else if (p_choice === 'paper' && c_choice === 'paper') {
+        alert(`Tie! Both You and the Computer Chose 'paper'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+    else if (p_choice === 'paper' && c_choice === 'scissor') {
+        computer_score =+ 1;
+        alert(`You Lose! You Chose 'paper' and the Computer Chose 'scissor'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+
+    else if (p_choice === 'scissor' && c_choice === 'rock') {
+        computer_score += 1;
+        alert(`You Lose! You Chose 'scissor' and the Computer Chose 'rock'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+    else if (p_choice === 'scissor' && c_choice === 'paper') {
+        player_score += 1;
+        alert(`You Win! You Chose 'scissor' and the Computer Chose 'paper'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+    else if (p_choice === 'scissor' && c_choice === 'scissor') {
+        alert(`Tie! Both You and the Computer Chose 'scissor'.\nYour score: ${player_score} | Computer score: ${computer_score}`);
+    }
+}
+
+CompareChoice(PlayerChoice(), CompChoice())
